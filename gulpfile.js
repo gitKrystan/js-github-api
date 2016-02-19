3a7447ec66791e0dd29ced4fe97b0e6d10458c50
Krystan Menne <kmenne@gmail.com>
2016-02-18 19:17:36 -0800

    add gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
 
 gulp.task('jshint', function() {
   return gulp.src(['js/*.js', 'spec/*.js', '*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
 });
