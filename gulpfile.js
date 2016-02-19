0d497aad7cd1ec87ea862e907ebdbe1265280c07
Krystan Menne <kmenne@gmail.com>
2016-02-18 19:36:40 -0800

    add bower

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,18 @@ var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
+var lib = require('bower-files')({
+  'overrides': {
+    'bootstrap': {
+      'main': [
+        'less/bootstrap.less',
+        'dist/css/boostrap.css',
+        'dist/js/bootstrap.js'
+      ]
+    }
+  }
+});
 
 gulp.task('jshint', function() {
   return gulp.src(['js/*.js', 'spec/*.js', '*.js'])
@@ -22,3 +34,24 @@ gulp.task('jsBrowserify', ['concatInterface'], function() {
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
 });
+
+gulp.task('minifyScripts', ['jsBrowserify'], function() {
+  return gulp.src('./build/js/app.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('jsBower', function() {
+  return gulp.src(lib.ext('js').files)
+    .pipe(concat('vendor.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('cssBower', function() {
+  return gulp.src(lib.ext('css').files)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('bowerProduction', ['jsBower', 'cssBower']);
